f6feaa1df30717c2999c931098679016ea563314
Kang Ki Tae <kt.kang@ridi.com>
2017-08-01 19:11:24 +0900

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# performance-apache-base
 Base Apache Docker Container for RIDI performance team
+========================================================
+
+[![](https://images.microbadger.com/badges/version/ridibooks/performance-apache-base.svg)](http://microbadger.com/images/ridibooks/performance-apache-base "Get your own version badge on microbadger.com")
+[![](https://images.microbadger.com/badges/image/ridibooks/performance-apache-base.svg)](http://microbadger.com/images/ridibooks/performance-apache-base "Get your own version badge on microbadger.com")
+
+Envionment variables
+--------
+- APACHE_DOC_ROOT - The document root is /var/www/html. (default) Set APACHE_DOC_ROOT if you want to change,
+- XDEBUG_ENABLE - if set this "1", then XDebug is enabled.
+- XDEBUG_HOST - XDebug host is computed at the start. You can specify a special host by this.
+- PHP_TIMEZONE - PHP time zone. (default = Asia/Seoul)
+
+Usage
+-----
+
+Standalone usage example with host's current working directory as document root:
+```
+docker run -p 80:80 \
+  -v $(pwd):/var/www/html \
+  --name php-apache \
+  -e XDEBUG_ENABLE=1 \
+  ridibooks/performance-apache-base
+```
---
+++ docker-entrypoint.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Configure PHP date.timezone
+echo "date.timezone = $PHP_TIMEZONE" > /usr/local/etc/php/conf.d/timezone.ini
+
+# Configure Apache Document Root
+mkdir -p $APACHE_DOC_ROOT
+chown www-data:www-data $APACHE_DOC_ROOT
+sed -i "s|DocumentRoot /var/www/html\$|DocumentRoot $APACHE_DOC_ROOT|" /etc/apache2/sites-available/000-default.conf
+echo "<Directory $APACHE_DOC_ROOT>" > /etc/apache2/conf-available/document-root-directory.conf
+echo "  AllowOverride All" >> /etc/apache2/conf-available/document-root-directory.conf
+echo "  Require all granted" >> /etc/apache2/conf-available/document-root-directory.conf
+echo "</Directory>" >> /etc/apache2/conf-available/document-root-directory.conf
+a2enconf "document-root-directory.conf"
+
+PHP_INI_PATH=/usr/local/etc/php/conf.d
+XDEBUG_DISABLED=$PHP_INI_PATH/99-xdebug.ini.disabled
+XDEBUG_ENABLED=$PHP_INI_PATH/99-xdebug.ini
+XDEBUG_REMOVE_HOST=$PHP_INI_PATH/xdebug_remote_host.ini
+
+# Enable XDebug if needed
+if [ "$XDEBUG_ENABLE" = "1" ]; then
+    if [ -f "$XDEBUG_DISABLED" ]; then
+        mv $XDEBUG_DISABLED $XDEBUG_ENABLED
+    fi;
+    # Configure XDebug remote host
+    if [ -z "$XDEBUG_HOST" ]; then
+        # Allows to set HOST_IP by env variable because could be different from the one which come from ip route command
+        HOST_IP=$(/sbin/ip route|awk '/default/ { print $3 }')
+    fi;
+    echo "xdebug.remote_host=$HOST_IP" > $XDEBUG_REMOVE_HOST
+else
+    if [ -f "$XDEBUG_ENABLED" ]; then
+        mv $XDEBUG_ENABLED $XDEBUG_DISABLED
+    fi;
+    if [ -f "$XDEBUG_REMOVE_HOST" ]; then
+      rm $XDEBUG_REMOVE_HOST
+    fi;
+fi;
+
+exec docker-php-entrypoint "$@"
